1140f9c2a2620dfa7b6098ed609378b172c36da0
Alexey Kabanin <alexey.kabanin@veeam.com>
2023-07-02 21:13:17 +0400

    updated

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const header = document.querySelector('.header');
+const navigationToggle = document.querySelector('.navigation__toggle');
+header.classList.add('header--js-enable');
+
+navigationToggle.addEventListener('click', () => {
+  header.classList.toggle('header--menu-open')
+})
