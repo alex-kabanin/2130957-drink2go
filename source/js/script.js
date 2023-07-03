ee3c29b9b78ff980239d797c156bc961d91dd95b
Alexey Kabanin <alexey.kabanin@veeam.com>
2023-07-03 16:11:51 +0400

    update

--- source/js/script.js
+++ source/js/script.js
@@ -6,3 +6,18 @@ header.classList.add('header--js-enable');
 navigationToggle.addEventListener('click', () => {
   header.classList.toggle('header--menu-open')
 })
+
+const map = L.map('map').setView([59.968322, 30.317359], 33);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+const customIcon = L.icon({
+  iconUrl: 'img/map/map_pin.svg',
+  iconSize: [38, 50],
+  iconAnchor: [22, 50],
+  popupAnchor: [-3, -76]
+});
+
+const marker = L.marker([59.968322, 30.317458], { icon: customIcon }).addTo(map);
+marker.bindPopup('Интернет-магазин кофейных напитков «Drink2Go»');
